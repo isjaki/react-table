ec06f8ebc46bcbf4005d0ab46070f59b71050ac4
isjaki <usow.aleksey@yandex.ru>
2019-04-10 00:13:28 +0400

    builded the project

---
+++ build/precache-manifest.c2210221e68a209aac4377d97aaefee5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9f641847bcd4b71b5b15",
+    "url": "/static/js/main.ff63d3a3.chunk.js"
+  },
+  {
+    "revision": "3eed836afa916ced58e9",
+    "url": "/static/js/2.4d7e578a.chunk.js"
+  },
+  {
+    "revision": "9f641847bcd4b71b5b15",
+    "url": "/static/css/main.b22e7816.chunk.css"
+  },
+  {
+    "revision": "cc8fdd38b9188e91226ff18f677b29bb",
+    "url": "/index.html"
+  }
+];
